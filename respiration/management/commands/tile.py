8c917b1e5abc0c48df9242175d6e451fa2d5858c
khagan <khagan@1f418930-7ff8-0310-b8ad-c653122473bc>
2009-03-13 20:47:10 +0000

    added hash template tag to forest
    git-svn-id: http://svn.ccnmtl.columbia.edu/blackrock/trunk@14120 1f418930-7ff8-0310-b8ad-c653122473bc

---
+++ respiration/management/commands/tile.py
@@ -0,0 +1,72 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.core import serializers
+from optparse import make_option
+from blackrock.sampler.models import *
+import csv
+
+class Command(BaseCommand):
+    
+    option_list = BaseCommand.option_list + (
+        make_option('--csv', dest='csv', help='Base CSV file to tile'),
+        make_option('--plotsize', dest='plotsize', help='Original plot size. Assuming square dimensions'),
+        make_option('--factor', dest='factor', help='The factor by which to multiply the plot size'),
+    )
+            
+    def handle(self, *app_labels, **options):
+        from django.db.models import get_app, get_apps, get_models
+        
+        args = 'Usage: python manage.py tile --csv csv file --plotsize original size --factor multiplication factor]'
+        
+        if (not options.get('csv') or not options.get('plotsize') or not options.get('factor')):
+            print args
+            return
+        
+        plotsize = int(options.get('plotsize'))
+        tiles = int(options.get('factor'))
+        
+        fh = open(options.get('csv'), 'r')
+        table = csv.reader(fh)
+        header = table.next()
+        
+        print '"ID","SPECIES","x","y","dbh"'
+
+        for i in range(len(header)):
+            h = header[i].lower()
+            if h == 'id':
+                id_idx = i
+            if h == 'species':
+                species_idx = i
+            if h == 'x':
+                x_idx = i
+            if h == 'y':
+                y_idx = i
+            if h == 'dbh':
+                dbh_idx = i
+                
+        trees = list(table)         
+        idmap = {}
+        plot = 1
+        
+        y = 0
+        while (y < tiles):
+            x = 0
+            while (x < tiles):
+                #adjust each tree's position according to the plot we're in
+                for tree in trees: 
+                    adjusted_id = plot * int(tree[id_idx])
+                    
+                    # verify this adjusted_id is not going to be a duplicate
+                    if (not idmap.has_key(adjusted_id)):
+                        idmap[adjusted_id] = 1
+                    else:
+                        print "ERROR: id overlap: %s. Contact your developer to fix the id adjustments" % adjusted_id
+                        return
+                         
+                    # figure out x/y offset, then add the tree's position
+                    adjusted_x = x * plotsize + float(tree[x_idx])
+                    adjusted_y = y * plotsize + float(tree[y_idx])
+                    print '%s,%s,%s,%s,%s' % (adjusted_id, tree[species_idx], adjusted_x, adjusted_y, tree[dbh_idx])
+                x += 1
+                plot += 42 # increment factor tested to ensure that no ids overlap up to a factor of 10. note that making plot odd is pretty key to this.
+            y += 1
+            
