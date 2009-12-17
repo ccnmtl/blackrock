09bde6393c8be70b03027fb309b01ab801181e02
khagan <khagan@1f418930-7ff8-0310-b8ad-c653122473bc>
2009-12-17 18:20:11 +0000

    data clean-up and framework for display
    git-svn-id: http://svn.ccnmtl.columbia.edu/blackrock/trunk@18265 1f418930-7ff8-0310-b8ad-c653122473bc

--- paleoecology/views.py
+++ paleoecology/views.py
@@ -13,11 +13,13 @@ def index(request, admin_msg=""):
 def identification(request):
   return render_to_response('paleoecology/identification.html')
 
-def coresample(request):
-  p = PollenType(name="test", type="A")
-  p.save()
-  pollen = PollenType.objects.all()
-  return render_to_response('paleoecology/coresample.html', {'pollen':pollen} )
+def explore(request):
+  samples = PollenSample.objects.all()
+  return render_to_response('paleoecology/core-explore.html', {'samples':samples} )
+
+def results(request):
+  samples = PollenSample.objects.all()
+  return render_to_response('paleoecology/core-results.html', {'samples':samples} )
 
 @user_passes_test(lambda u: u.is_staff)
 def getcsv(request):
@@ -38,42 +40,17 @@ def getcsv(request):
   #  writer.writerow(row)
   
   return response
-  
+
 @user_passes_test(lambda u: u.is_staff)
 def loadcounts(request):
-  # if csv file provided, load
-  if request.method == 'POST':
-    
-    try:
-      fh = request.FILES['csvfile']
-    except:
-      return HttpResponseRedirect("/paleoecology/")
+  return loadcsv(request, "counts")
 
-    if file == '':
-      # TODO: error checking (correct file type, etc.)
-      return HttpResponseRedirect("/paleoecology/")
-
-    table = csv.reader(fh)
-    headers = table.next()
-
-    for row in table:
-       for i in range(len(row)):
-         if i == 0: continue
-
-         (t, created) = PollenType.objects.get_or_create(name=headers[i])
-         (p, created) = PollenSample.objects.get_or_create(depth=row[0], pollen=t)
-         p.count = row[i]
-         t.save()
-         p.save()
-
-    admin_msg = "Successfully imported data."
-
-    return index(request, admin_msg)
-  
-  return HttpResponseRedirect("/paleoecology/")
-  
 @user_passes_test(lambda u: u.is_staff)
 def loadpercents(request):
+  return loadcsv(request, "percents")
+  
+@user_passes_test(lambda u: u.is_staff)
+def loadcsv(request, type):
   # if csv file provided, load
   if request.method == 'POST':
     
@@ -91,13 +68,20 @@ def loadpercents(request):
 
     for row in table:
        for i in range(len(row)):
-         if i == 0: continue
+         if i == 0: continue  # skip row[0], which is the depth
 
          (t, created) = PollenType.objects.get_or_create(name=headers[i])
          (p, created) = PollenSample.objects.get_or_create(depth=row[0], pollen=t)
-         p.percentage = row[i]
+         if p.depth == 10.0 and created:
+           print t.name
+
+         if type == "counts":
+           p.count = row[i]
+         else:
+           p.percentage = row[i]
+
+         t.save()
          p.save()
-         t.save()       
 
     admin_msg = "Successfully imported data."
 
