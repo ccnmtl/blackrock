10edeb6b6514226d0dcf25566405905c07cc53ca
khagan <khagan@1f418930-7ff8-0310-b8ad-c653122473bc>
2010-02-04 02:11:40 +0000

    removed click-on-div moves slider
    git-svn-id: http://svn.ccnmtl.columbia.edu/blackrock/trunk@18576 1f418930-7ff8-0310-b8ad-c653122473bc

--- paleoecology/media/js/explore-nav.js
+++ paleoecology/media/js/explore-nav.js
@@ -1,29 +1,22 @@
 var coreHeight = 0;
-var total = 1070;
 var zoomedHeight = 0;
 
-function moveSlider(e) {
-  if(e.src().id == "core-zoomed") {   // move from scroll
-    var scrolltop = $("core-zoomed").scrollTop;
-    var ratio = scrolltop / zoomedHeight;
-    setElementPosition("windowdrag", {'y':coreHeight * ratio});
-  }
-  else { // move to div
-    var depth = e.src().id.substr(11);
-    setElementPosition("windowdrag", {'y':oneCm * depth});
-  }
-  //$("windowdrag");
+function moveSlider() {
+  var scrolltop = $("core-zoomed").scrollTop;
+  var ratio = scrolltop / zoomedHeight;
+  setElementPosition("windowdrag", {'y':coreHeight * ratio});
 }
 
 function setupSlider() {
   connect("core-zoomed", "onscroll", moveSlider);
+
   forEach(getElementsByTagAndClassName("div", "core-slice"), function(elem) {
-    connect(elem, "onclick", moveSlider);
+    //connect(elem, "onclick", moveSlider);
     zoomedHeight += 18.5;  // each core slice is 18px tall.  18.5 works better... it is a mystery.
   });
   
-  coreHeight = getElementDimensions("core-unzoomed").h - 25;  // 25 is the height of the dragger
-  oneCm = coreHeight / total;  // size of 1cm of core sample in pixels
+  draggerHt = getElementDimensions("windowdrag").h;
+  coreHeight = getElementDimensions("core-unzoomed").h - draggerHt;
 }
 
 addLoadEvent(setupSlider);
