c0fdae00913c6574c1b9ab74397b609274aea62e
khagan <khagan@1f418930-7ff8-0310-b8ad-c653122473bc>
2010-02-04 23:29:10 +0000

    changed results page to CSV download
    git-svn-id: http://svn.ccnmtl.columbia.edu/blackrock/trunk@18584 1f418930-7ff8-0310-b8ad-c653122473bc

--- paleoecology/urls.py
+++ paleoecology/urls.py
@@ -2,18 +2,17 @@ from django.conf.urls.defaults import *
 import os.path
 
 media_root = os.path.join(os.path.dirname(__file__),"media")
+data_root = os.path.join(os.path.dirname(__file__),"data")
 
 urlpatterns = patterns('',
 	(r'^$', 'blackrock.paleoecology.views.index'),
         (r'^identification$', 'blackrock.paleoecology.views.identification'),
         (r'^explore$', 'blackrock.paleoecology.views.explore'),
-        (r'^results$', 'blackrock.paleoecology.views.results'),
-        (r'^getrow$', 'blackrock.paleoecology.views.getrow'),
+#        (r'^results$', 'blackrock.paleoecology.views.results'),
+#        (r'^getrow$', 'blackrock.paleoecology.views.getrow'),
         (r'^getpercents$', 'blackrock.paleoecology.views.getpercents'),
         (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': media_root}),
+        (r'^data/(?P<path>.*)$', 'django.views.static.serve', {'document_root': data_root}),
         (r'^loadcounts$', 'blackrock.paleoecology.views.loadcounts'),
         (r'^loadpercents$', 'blackrock.paleoecology.views.loadpercents'),
-#        (r'^loadcsv$', 'blackrock.paleoecology.views.loadcsv'),
-#        (r'^getcsv$', 'blackrock.paleoecology.views.getcsv'),
-#        (r'^getsum$', 'blackrock.paleoecology.views.getsum'),
 )
