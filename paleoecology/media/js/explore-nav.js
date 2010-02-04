5cc944d4de98b625471d93d8360796ae15765f87
khagan <khagan@1f418930-7ff8-0310-b8ad-c653122473bc>
2010-02-04 02:02:16 +0000

    working viewport but not draggable yet
    git-svn-id: http://svn.ccnmtl.columbia.edu/blackrock/trunk@18574 1f418930-7ff8-0310-b8ad-c653122473bc

--- paleoecology/media/js/explore-nav.js
+++ paleoecology/media/js/explore-nav.js
@@ -1,20 +1,25 @@
 var coreHeight = 0;
 var total = 1070;
+var zoomedHeight = 0;
 
-function moveSlider() {
+function moveSlider(e) {
+  if(e.src().id == "core-zoomed") {   // move from scroll
+    var scrolltop = $("core-zoomed").scrollTop;
+    var ratio = scrolltop / zoomedHeight;
+    setElementPosition("windowdrag", {'y':coreHeight * ratio});
+  }
+  else { // move to div
+    var depth = e.src().id.substr(11);
+    setElementPosition("windowdrag", {'y':oneCm * depth});
+  }
   //$("windowdrag");
-}
-
-function moveSliderToDiv(e) {
-  var depth = e.src().id.substr(11);
-  setElementPosition("windowdrag", {'y':oneCm * depth});
-  //$("windowdrag").top = oneCm * depth + "px";
 }
 
 function setupSlider() {
   connect("core-zoomed", "onscroll", moveSlider);
   forEach(getElementsByTagAndClassName("div", "core-slice"), function(elem) {
-    connect(elem, "onclick", moveSliderToDiv);
+    connect(elem, "onclick", moveSlider);
+    zoomedHeight += 18.5;  // each core slice is 18px tall.  18.5 works better... it is a mystery.
   });
   
   coreHeight = getElementDimensions("core-unzoomed").h - 25;  // 25 is the height of the dragger
