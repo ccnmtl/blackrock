ceba497b2cff55aa0d410d9d1ac495a69187457a
Zarina Mustapha <zarina@domU1.(none)>
2010-08-19 15:13:33 -0400

    views.py and style for the map img color

--- optimization/views.py
+++ optimization/views.py
@@ -561,7 +561,7 @@ def tree_png(request):
     dim = (parent.width+image_margin, parent.height+image_margin)
     im = Image.new("RGBA", dim) #alpha background
     for t in trees:
-      im.putpixel(( int((t.location.x-c.x)/MULTIPLIER), int((c.y-t.location.y)/MULTIPLIER)), (00,256,00))
+      im.putpixel(( int((t.location.x-c.x)/MULTIPLIER), int((c.y-t.location.y)/MULTIPLIER)), (00,204,00))
     response = HttpResponse(mimetype="image/png")
     im.crop( [0,0,int(parent.width),int(parent.height)] ).save(response, "PNG")
     #response['Cache-Control'] = 'max-age=%d'% 60*60*24*7
