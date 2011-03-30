f458d72717b06c9c159f0fc1be0a0687dfa8e1a4
Anders Pearson <anders@columbia.edu>
2011-03-30 13:10:06 -0400

    including blackrock.deploy_specific in INSTALLED_APPS breaks if it doesn't exist, so only include it if the import succeeds

--- settings_shared.py
+++ settings_shared.py
@@ -70,7 +70,6 @@ INSTALLED_APPS = (
     'blackrock.paleoecology',
     'blackrock.blackrock_main',
     'blackrock.portal',
-    'blackrock.deploy_specific',
     'django.contrib.databrowse',
     'gspreadsheet_importer',
     'haystack',
@@ -141,6 +140,8 @@ SESSION_COOKIE_AGE = 900
 #then you can put a settings.py file and templates/ overrides there
 try:
     from blackrock.deploy_specific.settings import *
+    INSTALLED_APPS = INSTALLED_APPS + ['blackrock.deploy_specific']
+
     if locals().has_key('EXTRA_INSTALLED_APPS'):
         INSTALLED_APPS = EXTRA_INSTALLED_APPS + INSTALLED_APPS
 except ImportError:
