ef73659394935fde0c479143d5cd553877fcf78a
Anders Pearson <anders@columbia.edu>
2011-04-11 14:47:47 -0400

    custom horizontal lines available for scatter plots as well

--- waterquality/views.py
+++ waterquality/views.py
@@ -41,6 +41,16 @@ def index(request):
     data["start"] = start
     data["end"] = end
 
+    if graph_type == 'time-series' or graph_type == 'scatter-plot':
+        lines = []
+        for k in request.GET.keys():
+            if k.startswith("line_value_"):
+                v = request.GET.get(k)
+                n = k[len("line_value_"):]
+                l = request.GET.get("line_label_%s" % n)
+                lines.append(dict(label=l,value=v,n=int(n)))
+        data["lines"] = lines
+
     if graph_type == 'time-series':
         series_ids = request.GET.getlist('series')
         datasets = []
@@ -62,14 +72,6 @@ def index(request):
         data['all_series'] = all_series
         data['show_graph'] = True
 
-        lines = []
-        for k in request.GET.keys():
-            if k.startswith("line_value_"):
-                v = request.GET.get(k)
-                n = k[len("line_value_"):]
-                l = request.GET.get("line_label_%s" % n)
-                lines.append(dict(label=l,value=v,n=int(n)))
-        data["lines"] = lines
     if graph_type == 'box-plot':
         series_ids = request.GET.getlist('series')
         datasets = []
