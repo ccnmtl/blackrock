6c22a53f14727b4e78ae815f91ce78e740d66985
Anders Pearson <anders@columbia.edu>
2011-11-01 14:48:58 -0400

    remove duplicates as well. PMT #77070

--- waterquality/management/commands/fill_in_hrecos.py
+++ waterquality/management/commands/fill_in_hrecos.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand
 from waterquality.models import Row,Series
 from datetime import timedelta
+from decimal import Decimal
 
 class Command(BaseCommand):
     args = ''
@@ -25,3 +26,39 @@ class Command(BaseCommand):
 
         print "filled in %d" % missing
         print "total: %d" % series.row_set.all().count()
+
+        print "checking for duplicates now..."
+        step_date = start_date
+        duplicates = 0
+        zero = Decimal("0.0")
+        while step_date < end_date:
+            r = series.row_set.filter(timestamp=step_date)
+            if r.count() > 1:
+                duplicates += r.count()
+                # need to pick which duplicate(s) to delete
+                # basically, if there's a non-zero, we prefer that one
+                # otherwise, just delete everything but the first
+                has_non_zero = False
+                for row in r:
+                    if row.value != zero:
+                        has_non_zero = True
+                if not has_non_zero:
+                    print "all zeros. delete all but first"
+                    for row in r[1:]:
+                        row.delete()
+                else:
+                    # clear out any zero values
+                    remaining = r.count()
+                    for row in r:
+                        if remaining > 1:
+                            if row.value == zero:
+                                row.delete()
+                                remaining = remaining - 1
+                    if remaining > 1:
+                        # more than one non-zero
+                        # delete everything but the first
+                        for row in r[1:]:
+                            row.delete()
+                    
+            step_date = step_date + d
+        print "duplicates found: %d" % duplicates
