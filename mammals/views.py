aa14dfb92792bb54bde3ac837207559887ca6e1d
Eddie Rubeiz <eddie@ccnmtl.columbia.edu>
2012-05-02 14:08:17 -0400

    adding csv export

--- mammals/views.py
+++ mammals/views.py
@@ -20,6 +20,7 @@ from blackrock.mammals.models import *
 from operator import attrgetter
 from string import uppercase
 from random import *
+import csv
 
 def get_float (request, name, default):
     number = request.POST.get(name, default)
@@ -97,14 +98,7 @@ def sandbox_grid(request):
 @csrf_protect
 @rendered_with('mammals/grid.html')
 def grid(request):
-
-
     grid = [gs.info_for_display() for gs in GridSquare.objects.all() if gs.display_this_square]
-    
-    #import pdb
-    #pdb.set_trace()
-    
-    
     return {
         'grid_json'                                  :  simplejson.dumps(grid)
         ,'grid_center_y'                             :  41.400
@@ -117,14 +111,9 @@ def grid(request):
 
 def pick_transects (center, side_of_square, number_of_transects, number_of_points_per_transect, magnetic_declination):
     result = []    
-    
     if number_of_transects > 15:
         number_of_transects = 15
-    
-    
-    
     new_transects = pick_transect_angles (number_of_transects)
-    
     for tr in new_transects:
         transect = {}
         transect_heading =  tr
@@ -139,8 +128,6 @@ def pick_transects (center, side_of_square, number_of_transects, number_of_point
         transect ['length'] = transect_length
         transect ['edge'] = walk (center, transect_length, transect_heading)
         points = []
-        
-        
         for j in range (number_of_points_per_transect):
             new_point = {}
             distance = triangular (0, transect_length, transect_length)
@@ -153,18 +140,17 @@ def pick_transects (center, side_of_square, number_of_transects, number_of_point
         result.append (transect)
     sorted_transects = sorted(result, key= lambda t: (t['heading']))
     transect_index, point_index = 0, 0
-    
-    
     for t in sorted_transects:
         transect_index += 1
         t['transect_id'] = transect_index
         t['team_letter'] = uppercase[transect_index - 1]
+        point_index_2 = 0
         for p in t['points']:
-            point_index += 1
-            p['point_id'] = point_index
+            point_index      += 1
+            point_index_2    += 1
+            p['point_id']    = point_index
+            p['point_index_2']  = point_index_2
             p['transect_id'] = transect_index
-          
-    
     return sorted_transects
     
 
@@ -174,7 +160,6 @@ def grid_block(request):
     default_lat = 41.400
     default_lon = -74.0305
     default_size = 250.0
-    
     if (request.method != 'POST'):
         #does it really make sense to just do a get on grid square? not really.
         num_transects                           = 2
@@ -187,15 +172,11 @@ def grid_block(request):
         grid_center_y, grid_center_x            = [default_lat, default_lon]
         selected_block_center_y, selected_block_center_x = block_center
     else:
-        
         num_transects        =                  get_int  ( request, 'num_transects',            2 )
         points_per_transect  =                  get_int  ( request, 'points_per_transect',      2 )
         radius_of_circles    =                  get_float( request, 'radius_of_circles',        30.0 )
         magnetic_declination =                  get_float( request, 'magnetic_declination',     -13.0 )
         block_size_in_m =                       get_float( request, 'block_size_in_m',        default_size)
-        
-        #import pdb
-        #pdb.set_trace()
         selected_block_center_y =               get_float( request, 'selected_block_center_y',  default_lat)
         selected_block_center_x =               get_float( request, 'selected_block_center_x',  default_lon)
         grid_center_y           =               get_float( request, 'grid_center_y',            default_lat)
@@ -203,16 +184,12 @@ def grid_block(request):
         height_in_blocks =                      get_int( request,   'height_in_blocks',         21)
         width_in_blocks   =                     get_int( request,   'width_in_blocks',          27)
         block_center = selected_block_center_y, selected_block_center_x
-        
-        
     block_height, block_width    = to_lat_long (block_size_in_m,  block_size_in_m )
-    
     transects = []
     transects = pick_transects (block_center, block_size_in_m, num_transects, points_per_transect, magnetic_declination)
 
     bottom_left = block_center[0] - (block_height / 2), block_center[1] - (block_width/2)
     block = set_up_block (bottom_left, block_height, block_width)
-
     return {
         'block_json': simplejson.dumps(block)
         ,'radius_of_circles'                         :  radius_of_circles # meters
@@ -229,11 +206,47 @@ def grid_block(request):
         ,'transects_json'                            :  simplejson.dumps(transects)
         ,'sandbox'                                   :  False
         ,'transects'                                 :  transects
-    
     }
     
-       
-#@rendered_with('mammals/sandbox_grid_block.html')
+def header_row():
+    return [
+                'Team letter'
+                , 'Point ID'
+                , 'Heading'
+                , 'Heading wrt magnetic north'
+                , 'Distance in meters'
+                , 'Latitude'
+                , 'Longitude'
+            ]
+
+def row_to_output (point, transect):
+    return [
+                transect['team_letter']
+                , point['point_index_2']
+                , transect['heading']
+                , transect['heading_wrt_magnetic_north']
+                , point['distance']
+                , point['point'][0]
+                , point['point'][1]
+            ]
+
+@csrf_protect
+def grid_square_csv(request):
+    # Create the HttpResponse object with the appropriate CSV header.
+
+    #import pdb
+    #pdb.set_trace()
+    transects_json = request.POST.get('transects_json')
+    obj = simplejson.loads(transects_json)
+    
+    response = HttpResponse(mimetype='text/csv')
+    response['Content-Disposition'] = 'attachment; filename=somefilename.csv'
+    writer = csv.writer(response)
+    writer.writerow(header_row())
+    for transect in obj:
+        for point in transect['points']:
+            writer.writerow(row_to_output(point, transect))
+    return response
 
 
 @csrf_protect
