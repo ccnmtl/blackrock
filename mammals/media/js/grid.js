8e01b2a4da6243c092cc6c939359acdad64c41d6
Eddie Rubeiz <eddie@ccnmtl.columbia.edu>
2012-05-29 17:50:38 -0400

    ok works much better

--- mammals/media/js/grid.js
+++ mammals/media/js/grid.js
@@ -1,5 +1,48 @@
 var grid_json;
 
+
+function addGrid(mapInstance) {
+    if (is_sandbox()) {
+         jQuery('#grid_form')[0].action =   '/mammals/sandbox/grid_square/';
+    } else {
+        jQuery('#grid_form')[0].action =   '/mammals/grid_square/';
+    }
+
+    var map_bounds = new google.maps.LatLngBounds();
+    grid_json = JSON.parse(jQuery('#grid_json')[0].innerHTML)
+    
+    
+    for (var i = 0; i < grid_json.length; i++) {
+        var box = grid_json[i]['corner_obj'];
+        var rect = make_grid_rectangle (bounds (box), mapInstance);
+        grid_json [i]['grid_rectangle'] = rect;
+        attach_info (grid_json[i]);
+        map_bounds.extend(lat_lng_from_point(box[4] ));
+        add_square_difficulty (i, grid_json [i]['access_difficulty']);
+        
+        jQuery ('#difficulty_menu_select').change (function (eee) {
+            show_squares (eee.currentTarget.value);
+        });
+    }
+    
+    
+    // this works but it's really annoying.
+    /*
+    viewer_location = user_location(mapInstance);
+    if (viewer_location ) {
+        you_are_here (viewer_location);
+    }
+    */
+    
+    if (!map_bounds.isEmpty() ) {
+        mapInstance.fitBounds(map_bounds);
+    } 
+}
+
+
+
+
+
 function box_info_from_grid_obj(obj) {
     //TODO: remove this adapter.
     return {
@@ -49,23 +92,27 @@ function random_item (an_array) {
     return an_array [random_index(an_array)];
 }
 
+function axe_square (sq) {
+    sq['grid_rectangle'].setOptions (square_styles['hidden']['unselected']  );
+    google.maps.event.clearListeners(sq['grid_rectangle'], 'mouseover');
+    google.maps.event.clearListeners(sq['grid_rectangle'], 'mouseout');
+}
+
+
 function show_squares (difficulty_level) {
     // show all the squares up to and including a particular level of difficulty of access.
     for (var i = 0; i < grid_json.length; i++) {
         sq = grid_json [i];
         if (sq ['access_difficulty'] > difficulty_level) {
             //console.log ('hiding square ' + i);
-            sq['grid_rectangle'].setOptions (square_styles['hidden']['unselected']  );
-            google.maps.event.clearListeners(sq['grid_rectangle'], 'mouseover');
-            google.maps.event.clearListeners(sq['grid_rectangle'], 'mouseout');
+            axe_square (sq);
         } else {
             sq['grid_rectangle'].setOptions (square_styles['regular']['unselected']  );
-            attach_info (sq['grid_rectangle'], box_info_from_grid_obj (sq));
+            attach_info (sq);
         }
     }
-
-
 }
+
 
 function pick_a_square (difficulty_level) {
     max_difficulty = 5;
@@ -82,53 +129,15 @@ function pick_a_square (difficulty_level) {
     return random_item(grid_json);
 }
 
-function addGrid(mapInstance) {
-    var map_bounds = new google.maps.LatLngBounds();
-    grid_json = JSON.parse(jQuery('#grid_json')[0].innerHTML)
-    
-    //alert (JSON.stringify(grid_json[0]));
-    //return;
-    
-    for (var i = 0; i < grid_json.length; i++) {
-        var box = grid_json[i]['corner_obj'];
-        var rect = make_grid_rectangle (bounds (box), mapInstance);
-        attach_info (rect, box_info_from_grid_obj (grid_json[i]));
-        map_bounds.extend(lat_lng_from_point(box[4] ));
-        grid_json [i]['grid_rectangle'] = rect
-        
-        //console.log (i);
-        //console.log (grid_json [i]['access_difficulty']);
-        
-        add_square_difficulty (i, grid_json [i]['access_difficulty']);
-            
-            jQuery ('#difficulty_menu_select').change (function (eee) {
-                show_squares (eee.currentTarget.value);
-                //show_squares( 1);
-            });
-            
-    }
-    
-    
-    // this works but it's really annoying.
-    /*
-    viewer_location = user_location(mapInstance);
-    if (viewer_location ) {
-        you_are_here (viewer_location);
-    }
-    */
-    
-    if (!map_bounds.isEmpty() ) {
-        mapInstance.fitBounds(map_bounds);
-    } 
-}
-
 
 
 function suggest_square() {
     // TODO: select only from squares of a particular length:
     suggested_square = pick_a_square (-1);
-    decorate_suggested_square (suggested_square.grid_rectangle);
     info = box_info_from_grid_obj(suggested_square);
+    
+    decorate_suggested_square (suggested_square);
+    //decorate_suggested_square (suggested_square.grid_rectangle);
     display_info_about_square (info)
 }
 
@@ -182,51 +191,47 @@ square_styles = {
 
 
 
-
-function attach_info(rect, info) {
-   /*
-   google.maps.clearListeners(rect, 'mouseover');
-   google.maps.clearListeners(rect, 'mouseout');
-   */
-    
+function attach_info(sq) {
+    rect = sq['grid_rectangle']
     add_regular_mouseover (rect);
     add_regular_mouseout (rect);
     google.maps.event.addListener(rect, 'mouseover', function() {
-        display_info_about_square (info);
+        display_info_about_square (box_info_from_grid_obj (sq));
     });
+    google.maps.event.clearListeners(rect, 'click');
     google.maps.event.addListener(rect, 'click', function() {
-     the_form = jQuery('#grid_form')[0]
-     if (is_sandbox()) {
-         the_form.action =   '/mammals/sandbox/grid_square/';
-    } else {
-        the_form.action =   '/mammals/grid_square/';
-    }
-     the_form.submit();
-  });
+        jQuery('#grid_form')[0].submit();
+    });
 }
 
-function decorate_suggested_square (suggested_square) {
-    
+function decorate_suggested_square (sq) {
+    rect = sq['grid_rectangle'];
     unsuggest_square();
-    suggested_square.setOptions (square_styles['suggested_square']['unselected']);
-    add_special_mouseout  (suggested_square);
-    add_special_mouseover (suggested_square);
-    
+    rect.setOptions (square_styles['suggested_square']['unselected']);
+    add_special_mouseout  (rect);
+    add_special_mouseover (rect);
     undecorate_suggested_square = function () {
-        suggested_square.setOptions (square_styles['regular']['unselected']);
-        add_regular_mouseover(suggested_square);
-        add_regular_mouseout(suggested_square);
+        attach_info(sq);
+        sq['grid_rectangle'].setOptions (square_styles['regular']['unselected']);
     }
 }
+
 
 function display_info_about_square (info) {
+
+    
+    //console.log ('Square # ' + info['label']);
+    
+    /// These two lines set the value of the square in the hidden form values.
+    jQuery('#selected_block_center_y') [0].value = info['box'][4][0];
+    jQuery('#selected_block_center_x') [0].value = info['box'][4][1];
+
+    // show values in the box:
     jQuery('#bl')[0].innerHTML = trimpoint(info['box'][0]);
     jQuery('#tl')[0].innerHTML = trimpoint(info['box'][1]);
     jQuery('#tr')[0].innerHTML = trimpoint(info['box'][2]);
     jQuery('#br')[0].innerHTML = trimpoint(info['box'][3]);
     jQuery('#c') [0].innerHTML = trimpoint(info['box'][4]);
-    jQuery('#selected_block_center_y') [0].value = info['box'][4][0];
-    jQuery('#selected_block_center_x') [0].value = info['box'][4][1];
     jQuery('#block_info')       [0].innerHTML =  'Square # ' + info['label']+ ':'
     jQuery('#block_difficulty') [0].innerHTML =  'Access difficulty: Level ' + info['access_difficulty']+ '.'
     jQuery('.grid_border_coords_table').show();
@@ -234,28 +239,28 @@ function display_info_about_square (info) {
 
 
 function add_regular_mouseover (rect) {
-    //google.maps.clearListeners(rect, 'mouseover'); /// hmm... this would turn off the display_info_about_square.
+    google.maps.event.clearListeners(rect, 'mouseover');
     google.maps.event.addListener(rect, 'mouseover', function() {
     rect.setOptions (square_styles['regular']['selected']);
   });
 }
 
 function add_regular_mouseout (rect) {
-  //google.maps.clearListeners(rect, 'mouseout');
+  google.maps.event.clearListeners(rect, 'mouseout');
   google.maps.event.addListener(rect, 'mouseout', function() {
     rect.setOptions (square_styles['regular']['unselected']);
   });
 }
 
 function add_special_mouseover (rect) {
-   //google.maps.clearListeners(rect, 'mouseover');
+    google.maps.event.clearListeners(rect, 'mouseover');
     google.maps.event.addListener(rect, 'mouseover', function() {
     rect.setOptions (square_styles['suggested_square']['selected']);
     });
 }
 
 function add_special_mouseout (rect) {
-   //google.maps.clearListeners(rect, 'mouseout');
+    google.maps.event.clearListeners(rect, 'mouseout');
     google.maps.event.addListener(rect, 'mouseout', function() {
     rect.setOptions (square_styles['suggested_square']['unselected']);
     });
