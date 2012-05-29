2e8671b378ded1cb40b8327afbcfd10c55a712ad
Eddie Rubeiz <eddie@ccnmtl.columbia.edu>
2012-05-29 12:39:50 -0400

    works but js leaks memory. need to clear events

--- mammals/media/js/grid.js
+++ mammals/media/js/grid.js
@@ -1,7 +1,7 @@
 var grid_json;
 
 function box_info_from_grid_obj(obj) {
-    //TODO: remove this adapter function.
+    //TODO: remove this adapter.
     return {
         'box':                 obj['corner_obj'],
         'row' :                obj['row'],
@@ -11,6 +11,76 @@ function box_info_from_grid_obj(obj) {
     }
 }
 
+
+
+difficulty_map = {}
+
+function add_square_difficulty(square_index, difficulty) {
+    //console.log ('adding_difficulty' + " square ix" + square_index + " diff: " + difficulty);
+    if (difficulty_map.hasOwnProperty (difficulty)) {
+        difficulty_map[difficulty].push (square_index);
+    }
+    else {
+        difficulty_map[difficulty] = [ square_index ];
+    }
+    //console.log (JSON.stringify(difficulty_map));
+
+}
+
+function eligible_squares (maximum_difficulty) {
+    result = [];
+    for (difficulty in difficulty_map) {
+        //console.log ("adding difficulty " + difficulty );
+        if (difficulty <= maximum_difficulty) {
+            //console.log ('adding');
+            result = result.concat (difficulty_map[difficulty])
+        }
+        //console.log ("eligible squares is now " +  JSON.stringify(result));
+    }
+    return result;
+}
+
+
+function random_index (an_array) {
+    return Math.floor(Math.random() * an_array.length)
+}
+
+function random_item (an_array) {
+    return an_array [random_index(an_array)];
+}
+
+function show_squares (difficulty_level) {
+    // show all the squares up to and including a particular level of difficulty of access.
+    for (var i = 0; i < grid_json.length; i++) {
+        sq = grid_json [i];
+        if (sq ['access_difficulty'] > difficulty_level) {
+            //console.log ('hiding square ' + i);
+            sq['grid_rectangle'].setOptions (square_styles['hidden']['unselected']  );
+            google.maps.event.clearListeners(sq['grid_rectangle'], 'mouseover');
+            google.maps.event.clearListeners(sq['grid_rectangle'], 'mouseout');
+        } else {
+            sq['grid_rectangle'].setOptions (square_styles['regular']['unselected']  );
+            attach_info (sq['grid_rectangle'], box_info_from_grid_obj (sq));
+        }
+    }
+
+
+}
+
+function pick_a_square (difficulty_level) {
+    max_difficulty = 5;
+    if (difficulty_level == -1) {
+        //alert ('showing all');
+        difficulty_level = max_difficulty;
+    }
+    else {
+        square_ids_to_choose_from = eligible_squares (difficulty_level);
+        the_id = random_item(square_ids_to_choose_from);
+        return grid_json[the_id];
+    }
+
+    return random_item(grid_json);
+}
 
 function addGrid(mapInstance) {
     var map_bounds = new google.maps.LatLngBounds();
@@ -25,6 +95,17 @@ function addGrid(mapInstance) {
         attach_info (rect, box_info_from_grid_obj (grid_json[i]));
         map_bounds.extend(lat_lng_from_point(box[4] ));
         grid_json [i]['grid_rectangle'] = rect
+        
+        //console.log (i);
+        //console.log (grid_json [i]['access_difficulty']);
+        
+        add_square_difficulty (i, grid_json [i]['access_difficulty']);
+            
+            jQuery ('#difficulty_menu_select').change (function (eee) {
+                show_squares (eee.currentTarget.value);
+                //show_squares( 1);
+            });
+            
     }
     
     
@@ -42,8 +123,10 @@ function addGrid(mapInstance) {
 }
 
 
+
 function suggest_square() {
-    suggested_square = grid_json[Math.floor(Math.random()*grid_json.length)];
+    // TODO: select only from squares of a particular length:
+    suggested_square = pick_a_square (-1);
     decorate_suggested_square (suggested_square.grid_rectangle);
     info = box_info_from_grid_obj(suggested_square);
     display_info_about_square (info)
@@ -56,24 +139,42 @@ function unsuggest_square() {
 }
 
 square_styles = {
-    'suggested_square' : {
+    'hidden' : {
+        'selected' : {
+            fillOpacity     : 0.0
+            ,strokeOpacity     : 0.0
+        }
+        ,'unselected' : {
+            fillOpacity     : 0.0
+            ,strokeOpacity     : 0.0
+        }
+    }
+    ,'suggested_square' : {
         'selected' : {
             fillOpacity     : 1.0
-           ,fillColor      : 'red'
+            ,fillColor      : 'red'
+            ,strokeOpacity   : 0.3
+            ,strokeColor     : 'green'
         }
         ,'unselected' : {
             fillOpacity     : 0.6
-           ,fillColor      : 'red'
+            ,fillColor      : 'red'
+            ,strokeOpacity   : 0.3
+            ,strokeColor     : 'green'
         }
     }
     ,'regular': {
         'selected' : {
             fillOpacity     : 0.3
-           ,fillColor      : 'blue'
+            ,fillColor      : 'blue'
+            ,strokeOpacity   : 0.3
+            ,strokeColor     : 'green'
         }
         ,'unselected' : {
             fillOpacity     : 0.1
-           ,fillColor      : 'blue'
+            ,fillColor      : 'blue'
+            ,strokeOpacity   : 0.3
+            ,strokeColor     : 'green'
         }
     }
 }
