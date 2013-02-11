78b02fa2921c4c853338875222474ddacb98415f
sdreher <sdreher@gmail.com>
2013-02-11 09:43:19 -0500

    Updating settings.

--- settings_production.py
+++ settings_production.py
@@ -1,6 +1,6 @@
 from settings_shared import *
 
-DATABASE_ENGINE = 'postgresql_psycopg2' # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+DATABASE_ENGINE = 'postgresql_psycopg2'
 
 TEMPLATE_DIRS = (
     "/var/www/blackrock/blackrock/templates",
@@ -10,16 +10,18 @@ MEDIA_ROOT = '/var/www/blackrock/uploads/'
 
 # put any static media here to override app served static media
 STATICMEDIA_MOUNTS = (
-    ('/sitemedia', '/var/www/blackrock/blackrock/sitemedia'),     
+    ('/sitemedia', '/var/www/blackrock/blackrock/sitemedia'),
 )
 
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
-HAYSTACK_SOLR_URL = "http://wwwapp.cc.columbia.edu/ccnmtl/solr/blackrock_portal"
+
+HAYSTACK_SITECONF = 'portal.search_sites'
+HAYSTACK_SEARCH_ENGINE = 'solr'
+HAYSTACK_SOLR_URL = \
+    "http://wwwapp.cc.columbia.edu/ccnmtl/solr/blackrock_portal"
 
 try:
     from local_settings import *
 except ImportError:
     pass
-
-
