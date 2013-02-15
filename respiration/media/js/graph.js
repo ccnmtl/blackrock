97c069202cd3029fe165f6cdf66f5be1f26ffa2e
sdreher <sdreher@gmail.com>
2013-02-15 10:34:22 -0500

    * respiration module - teaching resources tab, don't let the temperature go over 45 degrees, flake8 & jshint stuff, styling stuff

--- respiration/media/js/graph.js
+++ respiration/media/js/graph.js
@@ -13,21 +13,20 @@ function LeafGraphData() {
 var LeafData = new LeafGraphData();
 
 LeafGraphData.prototype.updateSpecies = function(species_id) {
-    //init
     if (typeof(this.species[species_id]) == 'undefined') {
       this.species[species_id] = {};
-      var color = this.species[species_id]['color'] = LeafData.colors.shift();
+      var color = this.species[species_id].color = LeafData.colors.shift();
       if( typeof( color ) == 'undefined' ) {  // if we run out of colors, cycle them
         LeafData.colors = ['#ff1f81', '#a21764', '#8ab438', '#999999', '#3a5b87', '#00c0c7', '#c070f0', '#ff8000', '#00ff00'];
-        this.species[species_id]['color'] = LeafData.colors.shift();
+        this.species[species_id].color = LeafData.colors.shift();
       }
     }
 
-    this.species[species_id]['name'] = $(species_id + "-name").value;
-    this.species[species_id]['R0'] = $(species_id + "-R0").value;
-    this.species[species_id]['E0'] = $(species_id + "-E0").value;
-    $(species_id + "-swatch").style.backgroundColor = this.species[species_id]['color'];
-}
+    this.species[species_id].name = $(species_id + "-name").value;
+    this.species[species_id].R0 = $(species_id + "-R0").value;
+    this.species[species_id].E0 = $(species_id + "-E0").value;
+    $(species_id + "-swatch").style.backgroundColor = this.species[species_id].color;
+};
 
 function updateColors() {
   forEach(getElementsByTagAndClassName('div', 'species'), function(species) {
@@ -36,66 +35,91 @@ function updateColors() {
 }
 
 LeafGraphData.prototype.updateFields = function() {
+    var min = ($('temp_low')) ? Math.round($('temp_low').value * 100) / 100 : 0;
+    var max = ($('temp_high')) ? Math.round($('temp_high').value * 100) / 100 : 30;
+    
+    if (max < 0 || max > 45) {
+        $('temp_high').focus();
+        alert("Please change the maximum temperature value. Valid temperature ranges are between 0C and 45C");
+        $('temp_high').value = 30;
+        return false;
+    } else if (min < 0 || min > 45) {
+        $('temp_low').focus();
+        alert("Please change the minimum temperature value. Valid temperature ranges are between 0C and 45C");
+        $('temp_low').value = 0;
+        return false;
+    }  else if (min > max) {
+        $('temp_low').focus();
+        alert("The minimum temperature must be less than the maximum temperature");
+        $('temp_low').value = 0;
+        $('temp_high').value = 30;
+        return false;
+    } 
+    
     this.t0 = Number($('kelvin').innerHTML);
-    this.t_a_min = ($('temp_low')) ? Math.round($('temp_low').value * 100) / 100 : 0;
-    this.t_a_max = ($('temp_high')) ? Math.round($('temp_high').value * 100) / 100 : 30;
-}
+    this.t_a_min = min;
+    this.t_a_max = max;
+    return true;
+};
 
 LeafGraphData.prototype.arrhenius = function(species_id, t_a) {
     var data = this.species[species_id];
     if ((! this.t0) || (isNaN(this.t0))) {
-	throw "Please set a valid base temperature.";
+    throw "Please set a valid base temperature.";
     }
-    if((! data['R0']) || (! data['E0']) || (isNaN(data['R0'])) || (isNaN(data['E0']))) {
-	throw "Please set valid R0 and E0 values for "+data.name;
+    if((! data.R0) || (! data.E0) || (isNaN(data.R0)) || (isNaN(data.E0))) {
+    throw "Please set valid R0 and E0 values for "+data.name;
     }
-    var Rval = arrhenius(data['R0'], data['E0'], this.Rg, this.t0, t_a+273.15);
+    var Rval = arrhenius(data.R0, data.E0, this.Rg, this.t0, t_a+273.15);
     return Rval;
-}
+};
 
 function leafGraph() {
     // have to re-init, because g.clear() doesn't reset legend
-    if($("plotGraph") == null) { return; }
-    removeElementClass('plotGraph','needsupdate');
+    if ($("plotGraph") === null) {
+        return false; 
+    }
+    if (!LeafData.updateFields()) {
+        return false;
+    }
+    removeElementClass('plotGraph', 'needsupdate');
     g = initGraph();
-    LeafData.updateFields();
+    
 
     forEach(getElementsByTagAndClassName('div', 'species'),
        function(species) {
-	   var spid = species.id;
-	   var data = [];
-	   //var color = colors.shift();
-	   LeafData.updateSpecies(spid);
+       var spid = species.id;
+       var data = [];
+       //var color = colors.shift();
+       LeafData.updateSpecies(spid);
 
-	   g.labels = {};
-	   //#54006 (LEAF) Remove x-axis labels...
-	   //g.labels[LeafData.t_a_min] = LeafData.t_a_min;
-	   //g.labels[LeafData.t_a_max] = LeafData.t_a_max;
-	   for(var i=LeafData.t_a_min; i<=LeafData.t_a_max; i++) {
-	       try {
-		   var Rval = LeafData.arrhenius(spid, i);
-		   if (!isNaN(Rval)) {
-		       data.push(Rval);
-		       //if(Rval > 0) { g.left_margin = 0; }
-		   }
-	       } catch(e) {
-		   g = initGraph();//re-init in case we already added data
-		   g.no_data_message = e;
-		   g.draw();
-		   throw "non valid data";
-	       }
-	   }
+       g.labels = {};
+       //#54006 (LEAF) Remove x-axis labels...
+       //g.labels[LeafData.t_a_min] = LeafData.t_a_min;
+       //g.labels[LeafData.t_a_max] = LeafData.t_a_max;
+       for(var i=LeafData.t_a_min; i<=LeafData.t_a_max; i++) {
+           try {
+           var Rval = LeafData.arrhenius(spid, i);
+           if (!isNaN(Rval)) {
+               data.push(Rval);
+               //if(Rval > 0) { g.left_margin = 0; }
+           }
+           } catch(e) {
+           g = initGraph();//re-init in case we already added data
+           g.no_data_message = e;
+           g.draw();
+           throw "non valid data";
+           }
+       }
 
 
-	   g.data(
-	     LeafData.species[spid].name,
-	     data, LeafData.species[spid]['color']
-	   );
+       g.data(LeafData.species[spid].name, data, LeafData.species[spid].color);
 
        });
   //g.data('Oaks', [1, 2, 3, 4, 4, 3]);
   //g.data('Maples', [4, 8, 7, 9, 8, 9]);
   g.draw();
+  return true;
 }
 
 
--- respiration/media/js/species.js
+++ respiration/media/js/species.js
@@ -6,18 +6,20 @@
     var speciesList = [];
     var html = "";
     function initSpeciesCloner() {
-    	html = $('species1').innerHTML;
+        html = $('species1').innerHTML;
     }
     
     function addSpecies(elem) {
       numSpecies++;
-      if(! elem) {
-        var scenario = document;
+      var scenario;
+      
+      if(!elem) {
+        scenario = document;
         speciesList.push("species" + numSpecies);
       }
       else {
         // get scenario, if there is one
-        var scenario = getFirstParentByTagAndClassName(elem, "div", "scenario");
+        scenario = getFirstParentByTagAndClassName(elem, "div", "scenario");
         if(! scenario) {
           scenario = document;
           speciesList.push("species" + numSpecies);
@@ -65,7 +67,7 @@
           $('species'+i+'-percent').value = $('leaf-species'+i+'-percent').value;
         }
       }
-      if(leafSpecies == 0) {setDefaults(); }
+      if(leafSpecies === 0) {setDefaults(); }
     }
 
     function setDefaults() {
@@ -99,179 +101,192 @@
     }
 
     function EquationHighlighter() {
-	connect(window,'onload',this,'onLoad');
-	this.vars = {"e-zero":[],
-		     "r-g":[],
-		     "t-zero":[],
-		     "t-a":[],
-		     "r-zero":[],
-		     "r-result":[]
-		    };
-	this.current = [];
+    connect(window,'onload',this,'onLoad');
+    this.vars = {"e-zero":[],
+             "r-g":[],
+             "t-zero":[],
+             "t-a":[],
+             "r-zero":[],
+             "r-result":[]
+            };
+    this.current = [];
     }
     EquationHighlighter.prototype.onLoad = function() {
-	var self = this;
-	this.arrhenius_vars = getElementsByTagAndClassName(null,'arr-variable','equation');
-	for (a in self.vars) {
-	    connect('arr-'+a,'onmouseenter',bind(self.hiliteFields,self,a));
-	    connect('arr-'+a,'onmouseleave',bind(self.unHiliteFields,self,a));
-	    forEach(getElementsByTagAndClassName(null,a),function(elt) {
-		connect(elt,'onfocus',bind(self.hiliteVar,self,a,addElementClass));
-		connect(elt,'onblur',bind(self.hiliteVar,self,a,removeElementClass));
-		if($("plotGraph") != null) {
-		  connect(elt,'onchange',self.needsUpdate);
-		}
-	    });
-	}
+    var self = this;
+    this.arrhenius_vars = getElementsByTagAndClassName(null,'arr-variable','equation');
+    for (var a in self.vars) {
+        connect('arr-'+a,'onmouseenter',bind(self.hiliteFields,self,a));
+        connect('arr-'+a,'onmouseleave',bind(self.unHiliteFields,self,a));
+        forEach(getElementsByTagAndClassName(null,a),function(elt) {
+        connect(elt,'onfocus',bind(self.hiliteVar,self,a,addElementClass));
+        connect(elt,'onblur',bind(self.hiliteVar,self,a,removeElementClass));
+        if($("plotGraph") !== null) {
+          connect(elt,'onchange',self.needsUpdate);
+        }
+        });
     }
+    };
+    
     EquationHighlighter.prototype.needsUpdate = function() {
-	//addElementClass('plotGraph','needsupdate');
-	leafGraph();
-	global.TemperatureSliders.updateGraphDimensions();
-    }
+        if (leafGraph()) {
+            global.TemperatureSliders.updateGraphDimensions();
+        }
+    };
+    
     EquationHighlighter.prototype.initSpecies = function(elt) {
-	var self = this;
-	e_zero = getFirstElementByTagAndClassName(null,"e-zero",elt);
-	r_zero = getFirstElementByTagAndClassName(null,"r-zero",elt);
-	connect(e_zero,'onfocus',function(){self.hiliteVar("e-zero");});
-	connect(r_zero,'onfocus',function(){self.hiliteVar("r-zero");});
-	if($("plotGraph") != null) {
-	  connect(e_zero,'onchange',self.needsUpdate);
-	  connect(r_zero,'onchange',self.needsUpdate);
-	}
-    }
+        var self = this;
+        e_zero = getFirstElementByTagAndClassName(null,"e-zero",elt);
+        r_zero = getFirstElementByTagAndClassName(null,"r-zero",elt);
+        connect(e_zero,'onfocus',function(){self.hiliteVar("e-zero");});
+        connect(r_zero,'onfocus',function(){self.hiliteVar("r-zero");});
+        if($("plotGraph") !== null) {
+          connect(e_zero,'onchange',self.needsUpdate);
+          connect(r_zero,'onchange',self.needsUpdate);
+        }
+    };
+    
     EquationHighlighter.prototype.hiliteVar = function(arr_var,select_func) {
-	var self = this;
-	forEach(self.arrhenius_vars,function(elt) {
-	    removeElementClass(elt,'selected');
-	});
-	select_func = (select_func)?select_func:addElementClass;
-	select_func('arr-'+arr_var,'selected');
-    }
+        var self = this;
+        forEach(self.arrhenius_vars,function(elt) {
+            removeElementClass(elt,'selected');
+        });
+        select_func = (select_func)?select_func:addElementClass;
+        select_func('arr-'+arr_var,'selected');
+    };
+    
     EquationHighlighter.prototype.hiliteFields = function(arr_var) {
-	var self = this;
-	self.curr = getElementsByTagAndClassName(null,arr_var);
-	forEach(self.curr,function(elt) {
-	    addElementClass(elt,'selected');
-	});
-	self.hiliteVar(arr_var);
-    }
+        var self = this;
+        self.curr = getElementsByTagAndClassName(null,arr_var);
+        forEach(self.curr,function(elt) {
+            addElementClass(elt,'selected');
+        });
+        self.hiliteVar(arr_var);
+    };
+    
     EquationHighlighter.prototype.unHiliteFields = function(arr_var) {
-	var self = this;
-	forEach(self.curr,function(elt) {
-	    removeElementClass(elt,'selected');
-	});
-    }
+        var self = this;
+        forEach(self.curr,function(elt) {
+            removeElementClass(elt,'selected');
+        });
+    };
 
     function TemperatureSliders() {
-	connect(window,'onload',this,'onLoad');
-	this.low=null;
-	this.high=null;
-	this.MAX_TEMP = 45;
-	this.MIN_TEMP_WIDTH = 10;
-	this.length=null;
-	this.margin = 25;
-	this.freeze = false;
+        connect(window,'onload',this,'onLoad');
+        this.low=null;
+        this.high=null;
+        this.MAX_TEMP = 45;
+        this.MIN_TEMP_WIDTH = 10;
+        this.length=null;
+        this.margin = 25;
+        this.freeze = false;
     }
     TemperatureSliders.prototype.onLoad = function() {
-	var self = this;
-	if(!$('temp-slider')) { return; }
-	try {
-  	  var sliders = getElementsByTagAndClassName(null,'slider','temp-slider');
-  	}
-  	catch(err) {
-  	  return;  // no sliders; don't initialize this part
-  	}
-	this.input_low = $('temp_low');
-	this.input_high = $('temp_high');
-	this.low = Number(this.input_low.value);
-	this.high = Number(this.input_high.value);
+        var self = this;
+        if(!$('temp-slider')) {
+            return; 
+        }
+        
+        var sliders = null;
+        try {
+            sliders = getElementsByTagAndClassName(null,'slider','temp-slider');
+        }
+        catch(err) {
+            return;  // no sliders; don't initialize this part
+        }
+        this.input_low = $('temp_low');
+        this.input_high = $('temp_high');
+        this.low = Number(this.input_low.value);
+        this.high = Number(this.input_high.value);
+    
+        this.length = getElementDimensions(sliders[0].parentNode).w - this.margin;
+        //length2temp:divide ; temp2length:multiply
+        this.conv = this.length/this.MAX_TEMP;
+        this.MIN_WIDTH = this.MIN_TEMP_WIDTH * this.conv;
 
-	this.length = getElementDimensions(sliders[0].parentNode).w - this.margin;
-	//length2temp:divide ; temp2length:multiply
-	this.conv = this.length/this.MAX_TEMP;
-	this.MIN_WIDTH = this.MIN_TEMP_WIDTH * this.conv;
+        this.canvas = getElement('graph');
+        this.graph_cursor = getElement('graph-cursor');
+        
+        /// Temperature Sliders
+        connect(this.input_low,'onchange',function(){self.low = self.input_low.value;
+                                 self.update('input_low');});
+        connect(this.input_high,'onchange',function(){self.high = self.input_high.value;
+                                  self.update('input_high');});
+        this.drag_low = Draggable(sliders[0],{snap:bind(this.lowSnap,this),
+                              onchange:bind(this.update,this,'drag_low'),
+                              revert:global.EquationHighlighter.needsUpdate
+                             });
+        this.drag_high = Draggable(sliders[1],{snap:bind(this.highSnap,this),
+                               onchange:bind(this.update,this,'drag_high'),
+                               revert:global.EquationHighlighter.needsUpdate
+                              });
+    
+        this.drag_range = Draggable(sliders[2],{snap:bind(this.rangeSnap,this),
+                            onchange:bind(this.update,this,'drag_range'),
+                            revert:global.EquationHighlighter.needsUpdate
+                               });
 
-	this.canvas = getElement('graph');
-	this.graph_cursor = getElement('graph-cursor');
-	
-	/// Temperature Sliders
-	connect(this.input_low,'onchange',function(){self.low = self.input_low.value;
-						     self.update('input_low')});
-	connect(this.input_high,'onchange',function(){self.high = self.input_high.value;
-						      self.update('input_high')});
-	this.drag_low = Draggable(sliders[0],{snap:bind(this.lowSnap,this),
-					      onchange:bind(this.update,this,'drag_low'),
-					      revert:global.EquationHighlighter.needsUpdate
-					     });
-	this.drag_high = Draggable(sliders[1],{snap:bind(this.highSnap,this),
-					       onchange:bind(this.update,this,'drag_high'),
-					       revert:global.EquationHighlighter.needsUpdate
-					      });
-
-	this.drag_range = Draggable(sliders[2],{snap:bind(this.rangeSnap,this),
-						onchange:bind(this.update,this,'drag_range'),
-						revert:global.EquationHighlighter.needsUpdate
-					       });
-
-	/// Graph Mouse Cursor
-	connect(this.canvas,'onmousemove',this,'graphCursor');
-	connect(this.canvas,'onclick',this,'setCursorFreeze');
-	connect('unfreeze','onclick',this,'setCursorFreeze');
-
-
-	this.update('do all of them');
-	global.EquationHighlighter.needsUpdate();
-    }
+        /// Graph Mouse Cursor
+        connect(this.canvas,'onmousemove',this,'graphCursor');
+        connect(this.canvas,'onclick',this,'setCursorFreeze');
+        connect('unfreeze','onclick',this,'setCursorFreeze');
+    
+    
+        this.update('do all of them');
+        global.EquationHighlighter.needsUpdate();
+    };
+    
     TemperatureSliders.prototype.lowSnap = function(x,y) {
-	x = Math.min(x,this.length-this.MIN_WIDTH);
-	x = Math.max(x,0);
-	this.low = x/this.conv; //math
-	this.high = Math.max(this.high,(x+this.MIN_WIDTH)/this.conv); //math
-	return [x,0];
-    }
+        x = Math.min(x,this.length-this.MIN_WIDTH);
+        x = Math.max(x,0);
+        this.low = x/this.conv; //math
+        this.high = Math.max(this.high,(x+this.MIN_WIDTH)/this.conv); //math
+        return [x,0];
+    };
+    
     TemperatureSliders.prototype.highSnap = function(x,y) {
-	x = Math.min(x,this.length);
-	x = Math.max(x,this.MIN_WIDTH);
-	this.low = Math.min(this.low,(x-this.MIN_WIDTH)/this.conv); //math
-	this.high = x/this.conv; //math
-	return [x,0];
-    }
+        x = Math.min(x,this.length);
+        x = Math.max(x,this.MIN_WIDTH);
+        this.low = Math.min(this.low,(x-this.MIN_WIDTH)/this.conv); //math
+        this.high = x/this.conv; //math
+        return [x,0];
+    };
+    
     TemperatureSliders.prototype.rangeSnap = function(x,y) {
-	var range = this.high - this.low;
-	x = Math.min(x,this.length - range*this.conv);
-	var low_coords = this.lowSnap(x,y);
-	this.high = this.low + range;
-	low_coords[1] = 8; //match #temp-slider .range in CSS
-	return low_coords;
-    }
+        var range = this.high - this.low;
+        x = Math.min(x,this.length - range*this.conv);
+        var low_coords = this.lowSnap(x,y);
+        this.high = this.low + range;
+        low_coords[1] = 8; //match #temp-slider .range in CSS
+        return low_coords;
+    };
 
     TemperatureSliders.prototype.update = function(not_me) {
-	var self = this;
-	var plans = {
-	    'input_low':function(){self.input_low.value = String(self.low).substr(0,4);},//2 decimals
-	    'input_high':function(){self.input_high.value = String(self.high).substr(0,4);},
-	    'drag_low':function(){
-		self.drag_low.element.style.left = self.low*self.conv +'px'; //math
-	    },
-	    'drag_high':function(){
-		self.drag_high.element.style.left = self.high*self.conv +'px'; //math
-	    },
-	    'drag_range':function(){
-		self.drag_range.element.style.left = self.low*self.conv +'px'; //math
-		self.drag_range.element.style.width = (self.high-self.low)*self.conv +'px'; //math
-	    }
-	};
-	delete plans[not_me];
-	for (a in plans) {plans[a]();}
-	//EquationHighlighter.needsUpdate();
-    }
+        var self = this;
+        var plans = {
+            'input_low':function(){self.input_low.value = String(self.low).substr(0,4);},//2 decimals
+            'input_high':function(){self.input_high.value = String(self.high).substr(0,4);},
+            'drag_low':function(){
+            self.drag_low.element.style.left = self.low*self.conv +'px'; //math
+            },
+            'drag_high':function(){
+            self.drag_high.element.style.left = self.high*self.conv +'px'; //math
+            },
+            'drag_range':function(){
+            self.drag_range.element.style.left = self.low*self.conv +'px'; //math
+            self.drag_range.element.style.width = (self.high-self.low)*self.conv +'px'; //math
+            }
+        };
+        delete plans[not_me];
+        for (var a in plans) {
+            plans[a]();
+        }
+    };
 
     TemperatureSliders.prototype.updateGraphDimensions = function() {
         this.graph_left_margin = this.calcGraphLeftMargin(); // variable based on the legend
         this.canvas_length = this.calcCanvasLength(); // depends on the graph_left_margin
         this.updateCursorVals();
-    }
+    };
     
     TemperatureSliders.prototype.calcGraphLeftMargin = function() {
         var canvas_position = getElementPosition(this.canvas);
@@ -285,7 +300,7 @@
         forEach(getElementsByTagAndClassName(null, "y-axis-label", parent),
                 function(elem) {
                     var coords = getElementPosition(elem);
-                    var dims = getElementDimensions(elem)
+                    var dims = getElementDimensions(elem);
                        
                     // This constant represents the left & right draw margins in the canvas
                     // Yes, ugly, but no way else to figure out the exact positions of the y-axis labels
@@ -296,51 +311,53 @@
                 });
 
         return margin;
-    }
+    };
     
     TemperatureSliders.prototype.calcCanvasLength = function() {
         var graph_right_margin = 1.75;
         var canvas_length = getElementDimensions(this.canvas).w - this.graph_left_margin - graph_right_margin;
         return canvas_length;
-    }
+    };
 
     TemperatureSliders.prototype.graphCursor = function(evt,do_anyway) {
-	if (this.freeze && !do_anyway) return;
-
-	var mouse = evt.mouse();
-	var coords = getElementPosition(this.canvas);
-	var pos_x = mouse.page.x - coords.x;
-	this.temp = pos_x - this.graph_left_margin;
-	if (this.temp >= this.canvas_length) { this.temp = this.canvas_length; }
-	if (this.temp >= 0) {
-	    this.graph_cursor.style.left = (pos_x)+'px';
-	    var self = this;
-	    setTimeout(function(){self.updateCursorVals();},10);
-	    setTimeout(function(){showElement(self.graph_cursor);},10);
-	}
-    }
+        if (this.freeze && !do_anyway) return;
+    
+        var mouse = evt.mouse();
+        var coords = getElementPosition(this.canvas);
+        var pos_x = mouse.page.x - coords.x;
+        this.temp = pos_x - this.graph_left_margin;
+        if (this.temp >= this.canvas_length) { this.temp = this.canvas_length; }
+        if (this.temp >= 0) {
+            this.graph_cursor.style.left = (pos_x)+'px';
+            var self = this;
+            setTimeout(function(){self.updateCursorVals();},10);
+            setTimeout(function(){showElement(self.graph_cursor);},10);
+        }
+    };
+    
     TemperatureSliders.prototype.setCursorFreeze = function(evt) {
-	var src = evt.src();
-	if (src.id == 'unfreeze') {
-	    this.freeze = false;
-	    hideElement(src);
-	}
-	else {
-	    this.graphCursor(evt,true);
-	    this.freeze = true;
-	    showElement('unfreeze');
-	}
-    }
+        var src = evt.src();
+        if (src.id == 'unfreeze') {
+            this.freeze = false;
+            hideElement(src);
+        }
+        else {
+            this.graphCursor(evt,true);
+            this.freeze = true;
+            showElement('unfreeze');
+        }
+    };
+    
     TemperatureSliders.prototype.updateCursorVals = function(evt) {
-	var lf = global.LeafData;
-	var real_temp = lf.t_a_min + (lf.t_a_max-lf.t_a_min)*this.temp/this.canvas_length;
-	if (!isNaN(real_temp)) {
-	    $('temp_mouse').value = Math.round(real_temp * 10) / 10;
-	    for (a in lf.species) {
-		$(a+'-R').value = lf.arrhenius(a,real_temp);
-	    }
-	}
-    }
+        var lf = global.LeafData;
+        var real_temp = lf.t_a_min + (lf.t_a_max-lf.t_a_min)*this.temp/this.canvas_length;
+        if (!isNaN(real_temp)) {
+            $('temp_mouse').value = Math.round(real_temp * 10) / 10;
+            for (var a in lf.species) {
+                $(a+'-R').value = lf.arrhenius(a,real_temp);
+            }
+        }
+    };
     
     function getNumSpecies() {
       return numSpecies;
