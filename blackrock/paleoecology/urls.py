e553e2738b28ad5a20b0c331ccb48501194050e9
Susan Dreher <sdreher@columbia.edu>
2014-08-07 15:37:09 -0400

    adding teaching resources tab for paleoecology
    Former-commit-id: 8fef0d1d819792e0ff43a6dc66b82299de49c900

--- blackrock/paleoecology/urls.py
+++ blackrock/paleoecology/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import patterns
+from django.views.generic.simple import direct_to_template
 import os.path
 
 media_root = os.path.join(os.path.dirname(__file__), "media")
@@ -7,8 +8,11 @@ data_root = os.path.join(os.path.dirname(__file__), "data")
 urlpatterns = patterns(
     '',
     (r'^$', 'blackrock.paleoecology.views.index'),
-    (r'^identification$', 'blackrock.paleoecology.views.identification'),
+    (r'^identification$', direct_to_template,
+     {'template': 'paleoecology/identification.html'}),
     (r'^explore$', 'blackrock.paleoecology.views.explore'),
+    (r'^resources$', direct_to_template,
+     {'template': 'paleoecology/resources.html'}),
     (r'^getpercents$', 'blackrock.paleoecology.views.getpercents'),
     (r'^media/(?P<path>.*)$', 'django.views.static.serve',
         {'document_root': media_root}),
