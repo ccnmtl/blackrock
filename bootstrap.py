d5fcf4352cf9f039ab4d88bd80676d35ed556de4
Anders Pearson <anders@columbia.edu>
2014-09-17 11:56:44 +0200

    fully convert to modern bootstrap + pypi mirror
    Former-commit-id: 96fe105ceeda4babb32b24122d321e3d78f3a481

--- bootstrap.py
+++ bootstrap.py
@@ -1,26 +1,22 @@
 #!/usr/bin/env python
-import glob
 import os
-import shutil
+import sys
 import subprocess
-
+import shutil
 
 pwd = os.path.abspath(os.path.dirname(__file__))
-vedir = os.path.abspath(os.path.join(pwd, "ve"))
+vedir = os.path.abspath(os.path.join(pwd,"ve"))
 
 if os.path.exists(vedir):
     shutil.rmtree(vedir)
 
-virtualenv_support_dir = os.path.abspath(
-    os.path.join(
-        pwd, "requirements", "virtualenv_support"))
+virtualenv_support_dir = os.path.abspath(os.path.join(pwd, "requirements", "virtualenv_support"))
 
-ret = subprocess.call(["python", "virtualenv.py",
+ret = subprocess.call(["python", "virtualenv.py", 
                        "--extra-search-dir=%s" % virtualenv_support_dir,
                        "--never-download",
                        vedir])
-if ret:
-    exit(ret)
+if ret: exit(ret)
 
 ret = subprocess.call(
     [os.path.join(vedir, 'bin', 'pip'), "install",
@@ -30,28 +26,14 @@ ret = subprocess.call(
 if ret:
     exit(ret)
 
-ret = subprocess.call(
-    [os.path.join(vedir, 'bin', 'pip'), "install",
-     "--use-wheel",
-     "--index-url=http://pypi.ccnmtl.columbia.edu/",
-     "--requirement", os.path.join(pwd, "requirements.txt")])
+ret = subprocess.call([os.path.join(vedir, 'bin', 'pip'), "install",
+                       "--use-wheel",
+                       "--index-url=http://pypi.ccnmtl.columbia.edu/",
+                       "--requirement",os.path.join(pwd,"requirements.txt")])
+if ret: exit(ret)
 
-if ret:
-    exit(ret)
-
-ret = subprocess.call(["python", "virtualenv.py", "--relocatable", vedir])
+ret = subprocess.call(["python","virtualenv.py","--relocatable",vedir])
 # --relocatable always complains about activate.csh, which we don't really
 # care about. but it means we need to ignore its error messages
 
 
-if ret:
-    exit(ret)
-
-# Copy a postgis adapter patch into the correct location
-pattern = "%s/lib/python*/site-packages/django/contrib/gis/db/backends/postgis/adapter.py" % vedir
-postgis_file = glob.glob(pattern)[0]
-ret = subprocess.call(['cp', 'requirements/src/adapter.py', postgis_file])
-if ret:
-    exit(ret)
-
-print "Postgres 9.1 postgis patch applied"
