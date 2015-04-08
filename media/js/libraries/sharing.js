fe4180b061dccb8d5cbf50e96674603e9d1e149b
Cassandra Dunlop <cdulop@columbia.com>
2015-04-08 13:16:01 -0400

    removing copy of sharing js from respiration

--- media/js/libraries/sharing.js
+++ media/js/libraries/sharing.js
@@ -8,7 +8,7 @@ jQuery(document).ready(function() {
       jQuery('#sharing_actions').Sharing({ 
          text: '',
          links: 'facebook,twitter,delicious,googlebookmarks,digg', 
-         image_dir: '/portal/media/js/jquery.sharing/sharing/',
+         image_dir: '/site_media/js/libraries/jquery.sharing/sharing/',
          image: 'http://upload.wikimedia.org/wikipedia/en/1/10/Black_Rock_Forest_logo.png', 
          title: 'I found this fabulous resource at the Black Rock Forest '
       });
