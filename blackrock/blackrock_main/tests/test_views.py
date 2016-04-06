b52c6b7c807d2736852e9d4a4b6f7d11be975beb
Anders Pearson <anders@columbia.edu>
2016-04-06 04:46:27 -0400

    :arrow_up: django-smoketest 1.0.0

--- blackrock/blackrock_main/tests/test_views.py
+++ blackrock/blackrock_main/tests/test_views.py
@@ -18,5 +18,4 @@ class SimpleViewTest(TestCase):
         # run the smoketests. we don't care if they pass
         # or fail, we just want to make sure that the
         # smoketests themselves don't have an error
-        response = self.c.get("/smoketest/")
-        self.assertEquals(response.status_code, 200)
+        self.c.get("/smoketest/")
