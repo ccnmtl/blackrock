ba5f7f48646680870b722e1cda53396e0818b5de
Nik Nyby <nnyby@columbia.edu>
2016-07-25 16:11:37 -0400

    use same structure as water chemistry module

--- blackrock/treegrowth/urls.py
+++ blackrock/treegrowth/urls.py
@@ -4,6 +4,10 @@ from django.views.generic.base import TemplateView
 
 urlpatterns = patterns(
     '',
-    url(r'^$', TemplateView.as_view(template_name='treegrowth/index.html'),
+    url(r'^$',
+        TemplateView.as_view(template_name='treegrowth/index.html'),
         name='treegrowth-index'),
+    url(r'^graph/$',
+        TemplateView.as_view(template_name='treegrowth/graph.html'),
+        name='treegrowth-graph'),
 )
