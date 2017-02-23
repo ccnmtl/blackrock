e38168120dd79b40a974f2e203188fe7a3a07efd
Nik Nyby <nnyby@columbia.edu>
2017-02-23 11:55:26 -0500

    Remove standard deviation from tree growth graph - PMT #110134

--- media/js/src/treegrowth/graph.js
+++ media/js/src/treegrowth/graph.js
@@ -17,11 +17,11 @@
             'Red_Oak_1_AVG', 'Red_Oak_2_AVG',
             'Red_Oak_3_AVG', 'Red_Oak_4_AVG',
             'Red_Oak_5_AVG',
-            'Site AVG', 'Site STD DEV',
+            'Site AVG',
             'White_Oak_1_AVG', 'White_Oak_2_AVG',
             'White_Oak_3_AVG', 'White_Oak_4_AVG',
             'White_Oak_5_AVG',
-            'Site AVG', 'Site STD DEV',
+            'Site AVG',
             'AvgTEMP_C', 'AvgVP', 'TotalRain',
             'SoilM_5cm', 'AvgPAR_Den'
         ];
@@ -29,14 +29,20 @@
             'S1 Oak 1', 'S1 Oak 2',
             'S1 Oak 3', 'S1 Oak 4',
             'S1 Oak 5',
-            'Site 1 AVG', 'Site 1 STD DEV',
+            'Site 1 AVG',
             'S2 Oak 1', 'S2 Oak 2',
             'S2 Oak 3', 'S2 Oak 4',
             'S2 Oak 5',
-            'Site 2 AVG', 'Site 2 STD DEV',
+            'Site 2 AVG',
             'AvgTEMP_C', 'AvgVP', 'TotalRain',
             'SoilM_5cm', 'AvgPAR_Den'
         ];
+
+        // Remove Site 1 STD DEV
+        data.splice(6, 1);
+        // Remove Site 2 STD DEV
+        data.splice(12, 1);
+
         for (var i = 0; i < data.length; i++) {
             var name = 'Series ' + (i + 1);
             if (i < headers.length) {
@@ -45,108 +51,53 @@
 
             var series = {};
             var yAxis = {};
-            if (name === 'Site STD DEV') {
-                var stdDevData = [];
-                for (var j = 0; j < data[i].length; j += 4) {
-                    var group = [];
-                    try {
-                        group = [
-                            data[i][j][0],
-                            data[i][j][1],
-                            data[i][j + 1][1],
-                            data[i][j + 2][1],
-                            data[i][j + 3][1]
-                        ];
-                    } catch (e) {
-                        continue;
-                    }
 
-                    var values = group.slice(1);
-                    var max = Math.max(...values);
-                    var min = Math.min(...values);
+            var isVisible = true;
+            if (name.indexOf('Red_Oak_') > -1 ||
+                name.indexOf('White_Oak_') > -1
+               ) {
+                isVisible = false;
+            }
 
-                    var candlestick = [
-                        group[0], // timestamp
-                        group[1], // open
-                        max,
-                        min,
-                        group[group.length - 1] // close
-                    ];
-                    stdDevData.push(candlestick);
-                }
-                series = {
-                    showInNavigator: false,
-                    type: 'candlestick',
-                    name: newHeaderNames[i],
-                    data: stdDevData,
-                    yAxis: i,
-                    dataGrouping: {
-                        units: [
-                            ['hour', [1, 8, 16]],
-                            ['day', [1, 7]]
-                        ]
-                    }
+            var showInNavigator = false;
+            if (i === 5) {
+                showInNavigator = true;
+            }
+
+            series = {
+                showInNavigator: showInNavigator,
+                name: newHeaderNames[i],
+                data: data[i],
+                yAxis: i,
+                visible: isVisible
+            };
+
+            // Environmental data has units
+            if (name === 'AvgTEMP_C') {
+                series.tooltip = {
+                    valueSuffix: '°C'
                 };
-                yAxis = {
-                    title: {
-                        text: name
-                    },
-                    top: '65%',
-                    height: '35%',
-                    visible: false
+            } else if (name === 'AvgVP') {
+                series.tooltip = {
+                    valueSuffix: ' kPa'
                 };
-            } else {
-                var isVisible = true;
-                if (name.indexOf('Red_Oak_') > -1 ||
-                    name.indexOf('White_Oak_') > -1
-                   ) {
-                    isVisible = false;
-                }
-
-                var showInNavigator = false;
-                if (i === 5) {
-                    showInNavigator = true;
-                }
-
-                series = {
-                    showInNavigator: showInNavigator,
-                    name: newHeaderNames[i],
-                    data: data[i],
-                    yAxis: i,
-                    visible: isVisible
+            } else if (name === 'TotalRain') {
+                series.tooltip = {
+                    valueSuffix: ' mm'
                 };
-
-                // Environmental data has units
-                if (name === 'AvgTEMP_C') {
-                    series.tooltip = {
-                        valueSuffix: '°C'
-                    };
-                } else if (name === 'AvgVP') {
-                    series.tooltip = {
-                        valueSuffix: ' kPa'
-                    };
-                } else if (name === 'TotalRain') {
-                    series.tooltip = {
-                        valueSuffix: ' mm'
-                    };
-                } else if (name === 'SoilM_5cm') {
-                    series.tooltip = {
-                        valueSuffix: ' VWC'
-                    };
-                } else if (name === 'AvgPAR_Den') {
-                    series.tooltip = {
-                        valueSuffix: ' umol/s/m²'
-                    };
-                }
-
-                yAxis = {
-                    title: {
-                        text: name
-                    },
-                    height: '65%',
-                    visible: false
+            } else if (name === 'SoilM_5cm') {
+                series.tooltip = {
+                    valueSuffix: ' VWC'
+                };
+            } else if (name === 'AvgPAR_Den') {
+                series.tooltip = {
+                    valueSuffix: ' umol/s/m²'
                 };
             }
+
+            yAxis = {
+                visible: false
+            };
 
             var pointFormatter = function() {
                 var unit = '';
@@ -170,29 +121,15 @@
         }
         $('#plot-container').highcharts('StockChart', {
             chart: {
-                height: 600,
-                plotBackgroundColor: {
-                    linearGradient: {
-                        x1: 0,
-                        y1: 0,
-                        x2: 0,
-                        y2: 1
-                    },
-                    stops: [
-                        [0, 'rgb(255, 255, 255)'],
-                        [0.65, 'rgb(255, 255, 255)'],
-                        [0.65, 'rgb(240, 240, 255)'],
-                        [1, 'rgb(240, 240, 255)']
-                    ]
-                }
+                height: 440
             },
             colors: [
                 // Site 1
                 '#FF8A80', '#EA80FC', '#FF1744', '#D500F9', '#C51162',
-                '#E91E63', '#880E4F',
+                '#E91E63',
                 // Site 2
                 '#80D8FF', '#A7FFEB', '#00B0FF', '#1DE9B6', '#00B8D4',
-                '#00BCD4', '#006064',
+                '#00BCD4',
                 // Environmental data
                 '#FF9800', '#FDD835', '#3D5AFE', '#795548', '#757575'
             ],
