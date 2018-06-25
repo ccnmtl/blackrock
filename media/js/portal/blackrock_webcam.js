599362d2e73d02f5ba38a77c03f0ded7bbc84406
Nick Buonincontri <nbuonin@gmail.com>
2018-06-25 10:49:42 -0400

    Resolve most eslint issues

--- media/js/portal/blackrock_webcam.js
+++ media/js/portal/blackrock_webcam.js
@@ -1,3 +1,5 @@
+/* eslint-disable security/detect-non-literal-fs-filename */
+/* eslint-disable security/detect-non-literal-regexp, no-useless-escape */
 /*******************************************************************************
  *
  * constructs an array of the blackrock static web cam images that we pull in
@@ -6,22 +8,21 @@
  ******************************************************************************/
 // query string processing - http://snipplr.com/view.php?codeview&id=13239
 function getParameterByName(name) {
-    name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
-    var regexS = "[\\?&]" + name + "=([^&#]*)";
+    name = name.replace(/[\[]/, '\\\[').replace(/[\]]/, '\\\]');
+    var regexS = '[\\?&]' + name + '=([^&#]*)';
     var regex = new RegExp(regexS);
     var results = regex.exec(window.location.href);
     if (results === null) {
-        return "";
+        return '';
     } else {
         return results[1];
     }
 }
 
 function get_webcam_image_array(start_date, start_month, start_year,
-        start_hour, start_minute) {
+    start_hour, start_minute) {
     var BASE_IMAGE_URL =
-        "https://www1.columbia.edu/sec/ccnmtl/projects/blackrock/forestdata/webcam";
-    var viewer;
+        'https://www1.columbia.edu/sec/ccnmtl/projects/blackrock/forestdata/webcam';
     var now = new Date();
     var interval = 10; // minutes
 
@@ -36,15 +37,15 @@ function get_webcam_image_array(start_date, start_month, start_year,
     start_month = start_month ||
         getParameterByName('month') ||
         now_date.getMonth();
-    
+
     start_year = start_year ||
         getParameterByName('year') ||
         now_date.getFullYear();
-    
+
     start_hour = start_hour ||
         getParameterByName('hour') ||
         now_date.getHours();
-    
+
     start_minute = start_minute ||
         getParameterByName('minute') ||
         now_date.getMinutes();
@@ -78,64 +79,64 @@ function get_webcam_image_array(start_date, start_month, start_year,
         // month in our script is 01-12, and starts at 1, not 0
         var month = current_min_date.getMonth() + 1;
         if (month < 10) {
-            month = "0" + month;
+            month = '0' + month;
         }
         // date is 0 padded in our script, so adjust
         var date = current_min_date.getDate();
         if (date < 10) {
-            date = "0" + date;
+            date = '0' + date;
         }
 
         var hour = current_min_date.getHours();
         if (hour < 10) {
-            hour = "0" + hour;
+            hour = '0' + hour;
         }
 
         var min = current_min_date.getMinutes();
         if (min < 10) {
-            min = "0" + min;
+            min = '0' + min;
         }
 
-        var path = "/" + year + "/" + month + "/" + date + "/";
-        var image_filename = "Black_Rock_" + hour + "_" + min + ".jpg";
+        var path = '/' + year + '/' + month + '/' + date + '/';
+        var image_filename = 'Black_Rock_' + hour + '_' + min + '.jpg';
         var full_image_path = BASE_IMAGE_URL + path + image_filename;
 
-        var thumb_filename = "Black_Rock_" + hour + "_" + min + "_thumb.jpg";
+        var thumb_filename = 'Black_Rock_' + hour + '_' + min + '_thumb.jpg';
         var full_thumb_path = BASE_IMAGE_URL + path + thumb_filename;
         imageObjArray.push({
             src: full_image_path,
             thumb: full_thumb_path,
             title: image_filename,
-            text: " Photo taken at " + hour + ":" + min
+            text: ' Photo taken at ' + hour + ':' + min
         });
     }
     return imageObjArray;
 }
 
 function draw_image_flow(start_date, start_month, start_year, start_hour,
-        start_minute) {
+    start_minute) {
     jQuery('#webcam-flow').empty();
 
     // add the webcam images to the dom
     var webcamImageArray = get_webcam_image_array(start_date, start_month,
-            start_year, start_hour, start_minute);
+        start_year, start_hour, start_minute);
     var idx = webcamImageArray.length - 1;
-    jQuery("#current_image").attr("src", webcamImageArray[idx].src);
-    jQuery("#current_image_text").html(webcamImageArray[idx].alt);
+    jQuery('#current_image').attr('src', webcamImageArray[idx].src);
+    jQuery('#current_image_text').html(webcamImageArray[idx].alt);
 
     for (var i = 0; i < webcamImageArray.length; i++) {
-        jQuery("<img />", {
-            "src": webcamImageArray[i].thumb,
-            "longdesc": webcamImageArray[i].thumb,
-            "width": "125",
-            "height": "85",
-            "alt": webcamImageArray[i].text
-        }).appendTo("#webcam-flow");
+        jQuery('<img />', {
+            'src': webcamImageArray[i].thumb,
+            'longdesc': webcamImageArray[i].thumb,
+            'width': '125',
+            'height': '85',
+            'alt': webcamImageArray[i].text
+        }).appendTo('#webcam-flow');
     }
 
-    var startIdx = jQuery("div#webcam-flow img").length;
+    var startIdx = jQuery('div#webcam-flow img').length;
 
-   
+
     var imageFlowInstance = new ImageFlow();
     imageFlowInstance.init({
         ImageFlowID: 'webcam-flow',
@@ -149,13 +150,13 @@ function draw_image_flow(start_date, start_month, start_year, start_hour,
         captions: false,
         reflections: false,
         reflectionP: 0.25,
-        onClick: function (img) {
+        onClick: function(img) {
             var fullUrl = img && img.src ? img.src: this.src;
-            jQuery("#current_image").attr("src",
-                    fullUrl.replace("_thumb", ""));
-            jQuery("#current_image_text").html(img.alt);
+            jQuery('#current_image').attr('src',
+                fullUrl.replace('_thumb', ''));
+            jQuery('#current_image_text').html(img.alt);
         }
     });
 
-    jQuery("#webcam-flow_key").show();
+    jQuery('#webcam-flow_key').show();
 }
