b933263642ba01fecc99d1095ae9eb3b782820aa
Susan Dreher <sdreher@gmail.com>
2022-05-31 11:06:57 -0400

    Short circuit data browse to display objects only
    django-databrowse is a great library for browsing object models. However, Blackrock doesn't really need all this functionality and there's not an easy way to tell the library exactly what we want. There's also some older code in django-databrowse that is sparking 500 errors. This PR circumvents the issues by doing a little url routing before handing the action back to databrowse.

--- blackrock/portal/urls.py
+++ blackrock/portal/urls.py
@@ -3,11 +3,11 @@ import os.path
 from django.conf.urls import url
 from django.views.generic.base import TemplateView
 import django.views.static
-import django_databrowse
+
 
 from blackrock.portal.search import PortalSearchView, PortalSearchForm
 
-from blackrock.portal.views import PortalPageView, nearby
+from blackrock.portal.views import PortalPageView, nearby, portal_databrowse
 
 
 media_root = os.path.join(os.path.dirname(__file__), "media")
@@ -15,10 +15,10 @@ media_root = os.path.join(os.path.dirname(__file__), "media")
 urlpatterns = [
     url(r'^media/(?P<path>.*)$', django.views.static.serve,
         {'document_root': media_root}),
-    url(r'^browse/(.*)', django_databrowse.site.root),
+    url(r'^browse/(.*)', portal_databrowse),
     url(r'^search/', PortalSearchView(
         template="portal/search.html",
-        form_class=PortalSearchForm), name='search'),
+        form_class=PortalSearchForm), name='portal-search'),
     url(r'^nearby/(?P<latitude>-?\d+\.\d+)/(?P<longitude>-?\d+\.\d+)/$',
         nearby),
     url(r'^weather/$', TemplateView.as_view(
