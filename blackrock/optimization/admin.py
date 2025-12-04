89ad61920a4a39ef1f968425e0e7b1c764df4168
Nikolas Nyby <nnyby@columbia.edu>
2025-12-04 16:02:39 -0500

    :arrow_up: Django 5.2

--- blackrock/optimization/admin.py
+++ blackrock/optimization/admin.py
@@ -1,5 +1,6 @@
 from django.contrib.gis import admin
+from django.contrib.gis.admin import GISModelAdmin
 from blackrock.optimization.models import Tree, Plot
 
-admin.site.register(Tree, admin.GeoModelAdmin)
-admin.site.register(Plot, admin.GeoModelAdmin)
+admin.site.register(Tree, GISModelAdmin)
+admin.site.register(Plot, GISModelAdmin)
